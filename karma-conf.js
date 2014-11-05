7b366d769204b30248d12731a5e5cf8a40f43c38
Ajai <ajainarayanan@gmail.com>
2014-11-04 19:35:12 -0800

    Fixes karma-conf.js and adds dependencies to package and bower json files

--- karma-conf.js
+++ karma-conf.js
@@ -2,9 +2,12 @@ module.exports = function(config) {
   var karma = {
     basePath: './modules/',
     files: [
-      '../bower_components/angular/angular.min.js',
+      '../bower_components/angular/angular.js',
       '../bower_components/angular-mocks/angular-mocks.js',
+      '../bower_components/jquery/dist/jquery.js',
 
+
+      '*/build/module.js',
       '*/build/*.js',
       '*/test/*.js'
     ],
