97450b69ab2274c0e76c86909584b7896cc972b6
Adrien Cahen <adrien@cask.co>
2014-11-25 22:24:58 -0800

    cask window-manager capsule

---
+++ modules/cask-angular-window-manager/wm.js
@@ -0,0 +1,93 @@
+angular.module('cask-angular-window-manager')
+
+  .constant('CASK_WM_EVENT', {
+    resize: 'cask-wm-resize',
+    blur: 'cask-wm-blur',
+    focus: 'cask-wm-focus'
+  })
+
+  .provider('caskWindowManager', function (CASK_WM_EVENT) {
+
+    this.resizeDebounceMs = 500;
+
+    this.$get = function ($rootScope, $window, $document, $log, $timeout) {
+
+      // resize inspired by https://github.com/danmasta/ngResize
+      var resizeDebounceMs = this.resizeThrottleMs,
+          resizePromise = null;
+
+      angular.element($window).on('resize', function (event) {
+        if(resizePromise) {
+          $timeout.cancel(resizePromise);
+        }
+        resizePromise = $timeout(function () {
+          $log.log('[caskWindowManager]', 'resize');
+          $rootScope.$broadcast(CASK_WM_EVENT.resize, {
+            width: $window.innerWidth,
+            height: $window.innerHeight
+          });
+        }, resizeDebounceMs, false);
+      });
+
+
+
+      // pageviz inspired by https://github.com/mz026/angular_page_visibility
+      var pageViz = {
+        hidden: 'visibilitychange',
+        mozHidden: 'mozvisibilitychange',
+        msHidden: 'msvisibilitychange',
+        webkitHidden: 'webkitvisibilitychange'
+      };
+
+      for (var p in pageViz) {
+        if (typeof ($document.prop(p)) !== 'undefined') {
+
+          $log.info('[caskWindowManager] page visibility API available!');
+
+          $document.on(pageViz[p], function (e) {
+            $log.log('[caskWindowManager]', e);
+            $rootScope.$broadcast(
+              CASK_WM_EVENT[ $document.prop(p) ? 'blur' : 'focus' ]
+            );
+          });
+
+          break;
+        } 
+      }
+
+      return {
+        event: CASK_WM_EVENT
+      };
+
+    };
+
+  })
+
+
+  /*
+  * caskOnWm Directive
+  *
+  * usage: cask-on-wm="{resize:expression()}"
+  * event data is available as $event
+  */
+  .directive('caskOnWm', function ($parse, $timeout, caskWindowManager) {
+    return {
+      compile: function ($element, attr) {
+        var obj = $parse(attr.caskOnWm);
+        return function (scope, element, attr) {
+          angular.forEach(obj, function (fn, key) {
+            var eName = caskWindowManager.event[key];
+            if(eName) {
+              scope.$on(eName, function (event, data) {
+                $timeout(function () {
+                  scope.$apply(function () {
+                    fn(scope, { $event: data });
+                  });
+                });
+              });
+            }
+          });
+        };
+      }
+    };
+  });
