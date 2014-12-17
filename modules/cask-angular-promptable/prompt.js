08c298042be9396a112d04325a96c9923b162b4c
Adrien Cahen <adrien@cask.co>
2014-12-16 17:56:08 -0800

    use same signature as window.prompt

--- modules/cask-angular-promptable/prompt.js
+++ modules/cask-angular-promptable/prompt.js
@@ -5,9 +5,8 @@
  *  the specified binding will be set to the user input 
  *  from a modal dialog. Eg:
  *
- * <a ng-click="caskPrompt('new '+model.name)"
+ * <a ng-click="caskPrompt('Please enter a new name', 'new '+model.name)"
  *       cask-promptable="model.name"
- *       data-prompt-title="Please enter a new name"
  * >rename</a>
  */
 
@@ -26,7 +25,7 @@ function caskPromptableDirective ($modal, caskFocusManager) {
 
       angular.extend(m.$scope, {
         value: '',
-        title: attrs.promptTitle || 'Prompt',
+        title: 'Prompt',
         setPromptable: function() {
           scope.$eval(attrs.caskPromptable + ' = ' + angular.toJson(m.$scope.value));
           m.hide();
@@ -41,9 +40,16 @@ function caskPromptableDirective ($modal, caskFocusManager) {
         caskFocusManager.select('caskPromptModal');
       });
 
-      scope.caskPrompt = function (v) {
-        if(!angular.isUndefined(v)) {
-          m.$scope.value = v;
+      scope.caskPrompt = function (text, prefill) {
+        if(!angular.isUndefined(text)) {
+          m.$scope.title = text;
+        }
+
+        if(!angular.isUndefined(prefill)) {
+          m.$scope.value = prefill;
+        }
+        else {
+          m.$scope.value = scope.$eval(attrs.caskPromptable);
         }
         m.show();
       };
