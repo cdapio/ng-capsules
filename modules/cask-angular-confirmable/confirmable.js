857635fd3afeb5a2960f2035270320f0f06b629a
Ajai <ajainarayanan@gmail.com>
2016-08-12 17:58:26 -0700

    Fixes parsing html content for confirmable modal and updates license in corresponding files

--- modules/cask-angular-confirmable/confirmable.js
+++ modules/cask-angular-confirmable/confirmable.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Cask Data, Inc.
+ * Copyright © 2015-2016 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -44,7 +44,7 @@ function caskConfirmableDirective ($modal, $sce) {
           modal.hide();
           scope.$eval(attrs.caskConfirmable);
         };
-        var confirmableContent = $sce.trustAsHtml(attrs.confirmableContent);
+        var confirmableContent = $sce.getTrustedHtml(attrs.confirmableContent);
         modal = $modal({
           scope: modalScope,
           template: 'cask-angular-confirmable/confirm-modal.html',
