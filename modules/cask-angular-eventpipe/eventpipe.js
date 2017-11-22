2bfa56f76f0e63c508439c88e92db7233a097278
Tony Bach <tbach@cask.co>
2017-11-22 10:56:28 -0800

    Adds code to remove a single cb in EventPipe, and corresponding test

--- modules/cask-angular-eventpipe/eventpipe.js
+++ modules/cask-angular-eventpipe/eventpipe.js
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Cask Data, Inc.
+ * Copyright © 2015-2017 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -24,6 +24,15 @@ angular.module('cask-angular-eventpipe')
       } else {
         events[event].push(cb);
       }
+      return function() {
+        if (!events[event]) {
+          return;
+        }
+        var index = events[event].indexOf(cb);
+        if (index !== -1) {
+          events[event].splice(index, 1);
+        }
+      };
     };
 
     this.emit =  function(event) {
@@ -41,7 +50,4 @@ angular.module('cask-angular-eventpipe')
         delete events[event];
       }
     };
-
-
-
   });
